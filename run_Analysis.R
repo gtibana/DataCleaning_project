74244b92dcba5e51edff22052be7a12910efb4c4
gtibana <67085847+gtibana@users.noreply.github.com>
2020-09-20 12:19:46 -0500

    Update run_Analysis.R

--- run_Analysis.R
+++ run_Analysis.R
@@ -1,23 +1,22 @@
-### Merge the test ans training datasets
+## Merge the test ans training datasets
 
-## 1. Load the librart
+# 1. Load the librart
 library(dplyr)
 
-## 2. work directory
+# 2. work directory
 if(!dir.exists("./data")) dir.create("./data")
 setwd("./data/")
 
-## 3. Download the data
+# 3. Download the data
 fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 destFile <- "UCI HAR Dataset.zip"
 download.file(fileUrl, destFile)
 
-## 4. Verify an decompress file
+# 4. Verify an decompress file
 if(file.exists(destFile))
         unzip(destFile)
 
-## 5. Read files
-
+# 5. Read the files
 features <- read.table("UCI HAR Dataset/features.txt", col.names = c("rownumber", "variablename"))
 # a. make variables tidy
 features <- mutate(features, variablename = gsub("BodyBody", "Body", variablename))
@@ -39,22 +38,19 @@ subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt", col.names
 x_train <- read.table("UCI HAR Dataset/train/X_train.txt", col.names = features$functions)
 y_train <- read.table("UCI HAR Dataset/train/y_train.txt", col.names = "code")
 
-#Merges the data
-#x_test
+# 6. Merges the data
 testData <- rbind(x_train, x_test)
-#y_test
 trainData <- rbind(y_train, y_test)
-#subject
 Subject <- rbind(subject_train, subject_test)
-#merge
 mergetesttrain <- cbind(Subject, trainData, testData)
 
-#filter by the mean and standard deviation
+# 7. Filter by mean and std
 filtermergetesttrain <- mergetesttrain %>% select(subject, code, contains("mean"), contains("std"))
-
-#Name the activities
 filtermergetesttrain$code <- activitieslabels[filtermergetesttrain$code, 2]
 
-#Average of each variable for each activity and each subject
+
+# 8. Average of activity and subject
 data <- filtermergetesttrain %>% group_by(subject, filtermergetesttrain$code) %>% summarise_all(funs(mean))
+
+# 9. Write the file#filter by the mean and standard deviation
 write.table(data, "Average_by_activityandsubject.txt", row.name=FALSE)
